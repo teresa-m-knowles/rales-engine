27d9f2bf41a6fa5fc31a0909b3c79254f508e0ec
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-11 23:18:14 -0600

    adds shoulda matchers config

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
   gem 'shoulda-matchers'
+  gem 'simplecov'
 end
 
 group :development do
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  it 'can create merchants from csv file' do
-    
+  describe 'validations' do
+    it {should validate_presence_of :name}
   end
 end
