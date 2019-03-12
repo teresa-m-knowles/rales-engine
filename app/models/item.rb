0c203111d0ad3de03eaf5073691ba962f62a4202
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-12 15:08:55 -0600

    adds tests for item relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :unit_price
+  validates_presence_of :created_at
+  validates_presence_of :updated_at
+
   belongs_to :merchant
-  has_many :invoiceitems
-  has_many :invoices, through: :invoiceitems
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end
