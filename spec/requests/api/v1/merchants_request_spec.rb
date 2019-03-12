f85fc0bd5b06c8d8964f9f29fc8a6586b86be367
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-11 23:30:53 -0600

    passes index test for merchants and creates show test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 describe 'Merchants API' do
   it "sends a list of merchants" do
-  create_list(:merchant, 3)
+    create_list(:merchant, 3)
 
-  get '/api/v1/merchants'
+    get '/api/v1/merchants'
+    merchants = JSON.parse(response.body)
 
-  expect(response).to be_successful
-end
+    expect(response).to be_successful
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'gets a merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
 end
