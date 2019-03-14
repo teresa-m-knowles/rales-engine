84a1483d501c5c9610c804572cb1a6f5aa4c09c1
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-14 01:50:05 -0600

    moved favorite customer test from merchant model spec to customer model spec. also added the method to the customer model as a class method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,14 @@ class Customer < ApplicationRecord
   validates_presence_of :last_name
   validates_presence_of :created_at
   validates_presence_of :updated_at
+
+  def self.favorite_customer(merchant_id)
+    Customer.joins(invoices: :transactions)
+            .where('invoices.merchant_id = ?', merchant_id)
+            .merge(Transaction.unscoped.successful)
+            .select('customers.*, count(transactions.id) as num_transactions')
+            .group('customers.id')
+            .order('num_transactions desc')
+            .first
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,4 +11,36 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it {should have_many :invoices}
   end
+
+  describe 'class methods' do
+    describe 'it returns the favorite customer, which is the customer who has conducted the most total number of successful transactions ' do
+      it 'favorite_customer' do
+        merchant = create(:merchant)
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+        #Customer 1 has 3 transactions, customer 2 has 1
+
+        item_1 = create(:item, unit_price: 10, merchant: merchant)
+        invoice_1 = create(:invoice, merchant: merchant, customer: customer_1)
+        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice_1)
+        transaction_1 = create(:transaction, invoice: invoice_1, result: 'success')
+
+        invoice_2 = create(:invoice, merchant: merchant, customer: customer_1)
+        invoice_item_2 = create(:invoice_item, item: item_1, invoice: invoice_2, quantity: 5)
+        transaction_2 = create(:transaction, invoice: invoice_1, result: 'success')
+
+        invoice_3 = create(:invoice, merchant: merchant, customer: customer_1)
+        item_2 = create(:item, unit_price: 23, merchant: merchant)
+        invoice_item_3 = create(:invoice_item, item: item_1, invoice: invoice_3)
+        transaction_3 = create(:transaction, invoice: invoice_1, result: 'success')
+
+        invoice_4 = create(:invoice, merchant: merchant, customer: customer_2)
+        invoice_item_3 = create(:invoice_item, item: item_2, invoice: invoice_4, quantity: 1)
+        transaction_3 = create(:transaction, invoice: invoice_3, result: 'success')
+
+        expect(Customer.favorite_customer(merchant.id)).to eq(customer_1)
+
+      end
+    end
+  end
 end
