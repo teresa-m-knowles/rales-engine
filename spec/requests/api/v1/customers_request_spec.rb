3fcf4bd106632bd3be50ef4a5ababe1fffc6c683
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-16 22:29:07 -0600

    customer show done

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe 'Customers API' do
 
     expect(result["data"].count).to eq(3)
   end
+
+  it 'shows one customer' do
+    customer = create(:customer)
+
+    get "/api/v1/customers/#{customer.id}"
+    expect(response).to be_successful
+
+    found_customer = JSON.parse(response.body)
+
+    expect(found_customer["data"]["id"]).to eq(customer.id.to_s)
+  end
 end
