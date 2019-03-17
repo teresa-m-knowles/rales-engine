f2d614416cbbfb77e093bddd8c91e971acc7515b
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-16 22:08:33 -0600

    items show action done

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -16,6 +16,18 @@ RSpec.describe 'Items API' do
     expect(indexed_items["data"].first["id"]).to eq(items.first.id.to_s)
     expect(indexed_items["data"].second["id"]).to eq(items[1].id.to_s)
     expect(indexed_items["data"].third["id"]).to eq(items[2].id.to_s)
+  end
 
+  it 'shows one item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+
+    get "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+
+    found_item = JSON.parse(response.body)["data"]
+
+    expect(found_item["attributes"]["id"]).to eq(item.id)
   end
 end
